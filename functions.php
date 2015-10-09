5086de58ba894857bd33ae1ca0d7b75a3917e09c
Hendrik Karilaid <karilaid@greeny.cs.tlu.ee>
2015-10-09 13:26:54 +0300

    added x

--- functions.php
+++ functions.php
@@ -20,24 +20,38 @@
 			$car = new StdClass();
 			$car->id = $id;
 			$car->number_plate = $number_plate;
-			
+			$car->color_from_db = $color_from_db;
+			$car->user_id = $user_id;
 			array_push($array, $car);
 			
 		}
 		
 		
 		
-		$row = 0;
+		//$row = 0;
 		//tee tsyklit nii mitu korda,kui saad ab-st yhe rea andmeid
-		while($stmt->fetch()){
+		/*while($stmt->fetch()){
 			echo $row.",".$number_plate."<br>";
 			$row = $row + 1;
 			
-		}
+		}*/
 		$stmt->close();
 		$mysqli->close();
 		
 		return $array;
 	}
-	
+	function deleteCar($id_to_be_deleted){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id_to_be_deleted);
+		
+		if($stmt->execute()){
+			
+			header("Location: table.php");
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+	}
 ?>
--- table.php
+++ table.php
@@ -1,6 +1,18 @@
 <?php
 	
 	require_once("functions.php");
+	
+	//kas kasutaja tahab kustutada
+	//vaatan kas aadressi real on ?delete=????
+	if(isset($_GET["delete"])){
+		
+		
+		deleteCar($_GET["delete"]);
+	}
+	
+	
+	
+	
 	$car_list = getCarData();
 	
 ?>
@@ -8,6 +20,21 @@
 <tr>
 	<th>ID</th>
 	<th>Auto nr märk</th>
+	<th>Värv</th>
+	<th>Kasutaja</th>
 </tr>
 
+	<?php
 	
+		for($i = 0; $i < count($car_list); $i++){
+			echo "<tr>";
+			
+			echo "<td>".$car_list[$i]->id."</td>";
+			echo "<td>".$car_list[$i]->number_plate."</td>";
+			echo "<td>".$car_list[$i]->color_from_db."</td>";
+			echo "<td>".$car_list[$i]->user_id."</td>";
+			echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
+			
+			echo "<tr>";
+			
+		}
