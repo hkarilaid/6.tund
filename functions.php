36fab075eaeefbacce5d03547fcf75fad85ab854
Hendrik Karilaid <karilaid@greeny.cs.tlu.ee>
2015-10-09 12:40:34 +0300

    added table

--- functions.php
+++ functions.php
@@ -1,5 +1,43 @@
 <?php
 	
-	//functions.php
-
+	require_once("../configglobal.php");
+	$database = "if15_karilaid";
+	
+	//Loome uue funktsiooni, et kysida ab-st andmeid
+	function getCarData(){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+		$stmt->bind_result($id, $user_id, $number_plate, $color_from_db);
+		$stmt->execute();
+		
+		//tyhi massiiv kus hoiame objekte
+		$array = array();
+		
+		while($stmt->fetch()){
+			
+			$car = new StdClass();
+			$car->id = $id;
+			$car->number_plate = $number_plate;
+			
+			array_push($array, $car);
+			
+		}
+		
+		
+		
+		$row = 0;
+		//tee tsyklit nii mitu korda,kui saad ab-st yhe rea andmeid
+		while($stmt->fetch()){
+			echo $row.",".$number_plate."<br>";
+			$row = $row + 1;
+			
+		}
+		$stmt->close();
+		$mysqli->close();
+		
+		return $array;
+	}
+	
 ?>
--- table.php
+++ table.php
@@ -1,5 +1,13 @@
 <?php
 	
-	//table.php
-
+	require_once("functions.php");
+	$car_list = getCarData();
+	
 ?>
+<table border=1 >
+<tr>
+	<th>ID</th>
+	<th>Auto nr märk</th>
+</tr>
+
+	
